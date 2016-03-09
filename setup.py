74c219365f61c0a16a185d0353451f064e015529
Fabio Zanini <fabio.zanini@tuebingen.mpg.de>
2016-03-09 02:58:20 +0100

    Style changes and tested for Python 3.5

--- setup.py
+++ setup.py
@@ -13,30 +13,30 @@ from distutils.core import setup, Extension
 seqan_path = '/usr/include'
 
 # Extension description
-seqan_module = Extension('_seqanpy',
-                         sources=['seqanpy.i',
-                                  'test.cpp',
-                                  'align.cpp'],
-                         swig_opts = ['-c++',
-                                      '-modern',
-                                      '-modernargs',
-                                      '-keyword',
-                                      '-I'+seqan_path],
-                         include_dirs=[seqan_path],
-                        )
+_seqanpy = Extension('_seqanpy',
+                     sources=['seqanpy.i',
+                              'test.cpp',
+                              'align.cpp'],
+                     swig_opts = ['-c++',
+                                  '-modern',
+                                  '-modernargs',
+                                  '-keyword',
+                                  '-I'+seqan_path],
+                     include_dirs=[seqan_path],
+                    )
 
 
-setup (name = 'seqanpy',
-       version = '0.1',
-       author      = "Fabio Zanini",
-       description = """Python wrapper of some SeqAn functions (for now just pairwise alignments)""",
-       ext_modules = [seqan_module],
-       py_modules = ["seqanpy"],
+setup(name='seqanpy',
+      version='0.1',
+      author="Fabio Zanini",
+      description="""Python wrapper of some SeqAn functions (for now pairwise alignments)""",
+      ext_modules=[_seqanpy],
+      py_modules=["seqanpy"],
 
-       # metadata for upload to PyPI
-       author_email = "fabio.zanini@tuebingen.mpg.de",
-       license = "BSD/2-clause",
-       keywords = "alignment sequence pairwise C++",
-       url = "http://example.com/HelloWorld/",
-       )
+      # metadata for upload to PyPI
+      author_email="fabio.zanini@fastmail.fm",
+      license="BSD/2-clause",
+      keywords="alignment sequence pairwise C++",
+      url="https://github.com/iosonofabio/seqanpy",
+     )
 
